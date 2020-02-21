383b0ebba446b9dea9f7c1851b8e2cf71951c044
loyal-transpiler-0513 <ifeawoyale@yahoo.com>
2020-02-21 02:32:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -43,5 +43,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length -1)
+  return array.slice(array.length -1)
 }
